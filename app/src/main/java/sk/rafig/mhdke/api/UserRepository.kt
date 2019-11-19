09321a1a2ac802e9aeac5e0d1406ffa378efa9c1
Radoslav Figura <figura.radoslav@gmail.com>
2019-11-19 06:45:03 +0100

    - added repo

---
+++ app/src/main/java/sk/rafig/mhdke/api/UserRepository.kt
@@ -0,0 +1,18 @@
+package sk.rafig.mhdke.api
+
+import androidx.lifecycle.LiveData
+import sk.rafig.mhdke.model.User
+import sk.rafig.mhdke.util.ContextTags
+
+class UserRepository(private val userDao: UserDao) {
+    lateinit var user: LiveData<User>
+
+    suspend fun createPerson(user: User){
+        userDao.addUser(user)
+    }
+
+    fun getPerson(id: Int): LiveData<User> {
+        user = userDao.getUser(id)
+        return user
+    }
+}
